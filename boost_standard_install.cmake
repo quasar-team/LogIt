846a4f4c89d87e688611f3c5bdc9fad5feb9848b
Benjamin Farnham <bfarnham@dev-x64-cc7.cern.ch>
2020-01-29 15:35:22 +0100

    build boost as stand-alone shared or stand-alone static (or, as part of a larger CMake project, e.g. a quasar server

--- boost_standard_install.cmake
+++ boost_standard_install.cmake
@@ -16,31 +16,22 @@
 
 # Authors:
 # Ben Farnham <firstNm.secondNm@cern.ch>
-cmake_minimum_required( VERSION 2.8 )
-#project( FindSystemBoost CXX )
-message(STATUS "Using file [boost_standard_install_cc7.cmake] toolchain file")
+message( STATUS "using file [boost_standard_install.cmake] with BOOST_ROOT environment variable set to [$ENV{BOOST_ROOT}]. Using FindBoost." )
 
-message(STATUS "CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES [${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES}]")
+set( BOOST_LIBS_TO_FIND chrono )
+if( LOGIT_BACKEND_BOOSTLOG )
+  set( BOOST_LIBS_TO_FIND ${BOOST_LIBS_TO_FIND} log log_setup )
+endif()
 
-#
-# Warning: *Distinctly* suspect that these should require be set but otherwise
-# no boost libs are found. Possibly fixed in some later version, but not in
-# cmake v2.8's FindBoost package
-#
-set(BOOST_INCLUDEDIR "/usr/include/")
-set(BOOST_LIBRARYDIR "/usr/lib64/")
+message( STATUS "Looking for boost libs [${BOOST_LIBS_TO_FIND}]" )
+find_package( Boost REQUIRED COMPONENTS ${BOOST_LIBS_TO_FIND} )
 
-set(CMAKE_FIND_LIBRARY_SUFFIXES ".so")
-set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
+if( NOT Boost_FOUND )
+  message( FATAL_ERROR "Failed to find system boost installation (BOOST_ROOT environment variable correctly set ? value [$ENV{BOOST_ROOT}]" )
+endif()
 
-find_package(Boost REQUIRED
-	program_options system filesystem chrono date_time thread)
-
-if(NOT Boost_FOUND)
-	message(FATAL_ERROR "Failed to find system boost installation (is the boost-devel package installed on this machine?)")
-else()
-	message(STATUS "Found system boost, version [${Boost_VERSION}], include dir [${Boost_INCLUDE_DIRS}] library dir [${Boost_LIBRARY_DIRS}], libs [${Boost_LIBRARIES}]")
-endif()	
-
-include( ${Boost_INCLUDE_DIRS} )
-set( BOOST_LIBS ${Boost_LIBRARIES} )
+message( STATUS "Boost_VERSION [${Boost_VERSION}] Boost_VERSION_STRING [${Boost_VERSION_STRING}]" )
+set( BOOST_LIBS Boost::chrono )
+if( LOGIT_BACKEND_BOOSTLOG )
+  set( BOOST_LIBS ${BOOST_LIBS} Boost::log Boost::log_setup )
+endif()
