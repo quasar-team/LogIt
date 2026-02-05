71a9a0f5fb270c7520a9b6469c42361139e93c21
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-02-05 14:55:32 +0100

    Add support Unified Automation on Linux

---
+++ cmake/LogItBackendBoostlog.cmake
@@ -0,0 +1,26 @@
+list(APPEND LOGIT_SOURCES src/BoostRotatingFileLog.cpp)
+list(APPEND LOGIT_DEFINITIONS LOGIT_BACKEND_BOOSTLOG)
+
+set(BOOST_HOME "$ENV{BOOST_HOME}")
+if(NOT BOOST_HOME)
+    message(FATAL_ERROR "LOGIT_BACKEND_BOOSTLOG requires BOOST_HOME to be set (e.g. /boost with include/ and lib/).")
+endif()
+
+set(BOOST_INCLUDE_DIR "${BOOST_HOME}/include")
+set(BOOST_LIBRARY_DIR "${BOOST_HOME}/lib")
+if(NOT EXISTS "${BOOST_INCLUDE_DIR}")
+    message(FATAL_ERROR "BOOST_HOME does not contain include/: ${BOOST_INCLUDE_DIR}")
+endif()
+if(NOT EXISTS "${BOOST_LIBRARY_DIR}")
+    message(FATAL_ERROR "BOOST_HOME does not contain lib/: ${BOOST_LIBRARY_DIR}")
+endif()
+
+list(APPEND LOGIT_EXTRA_INCLUDE_DIRS "${BOOST_INCLUDE_DIR}")
+foreach(_boost_lib boost_log)
+    find_library(BOOST_${_boost_lib}_LIB NAMES "lib${_boost_lib}.a" PATHS "${BOOST_LIBRARY_DIR}" NO_DEFAULT_PATH)
+    if(NOT BOOST_${_boost_lib}_LIB)
+        message(FATAL_ERROR "Missing static Boost library lib${_boost_lib}.a in ${BOOST_LIBRARY_DIR}")
+    endif()
+    list(APPEND LOGIT_EXTRA_LINK_LIBS "${BOOST_${_boost_lib}_LIB}")
+endforeach()
+
---
+++ cmake/LogItOptions.cmake
@@ -0,0 +1,6 @@
+option(LOGIT_BACKEND_STDOUTLOG "The basic back-end: logs to stdout" ON)
+option(LOGIT_BACKEND_BOOSTLOG "Rotating file logger back-end: fixed size on disk based on boost logging library" OFF)
+option(LOGIT_BACKEND_UATRACE "UnifiedAutomation toolkit logger" OFF)
+option(LOGIT_BACKEND_WINDOWS_DEBUGGER "Windows debugger logger" OFF)
+option(LOGIT_BUILD_SHARED_LIB "Build a LogIt shared library if ON (otherwise builds an object library)" OFF)
+
