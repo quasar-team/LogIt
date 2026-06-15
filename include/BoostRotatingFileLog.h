e482e9dc5e499e4f2aaaf2b0d20258678418176b
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-06-15 08:36:58 +0200

    Add missing override to the other backends (#18)

--- include/BoostRotatingFileLog.h
+++ include/BoostRotatingFileLog.h
@@ -29,8 +29,8 @@
 class BoostRotatingFileLog : public LogSinkInterface
 {
 public:
-    virtual bool initialize();
-    virtual void logMessage(const std::string& msg);
+    virtual bool initialize() override;
+    virtual void logMessage(const std::string& msg) override;
 
 private:
     void addFileSink();
