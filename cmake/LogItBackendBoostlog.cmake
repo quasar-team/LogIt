4c6679c004b3cf303b8ee5336d7014520ce78f86
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-06-30 14:22:33 +0200

    Enhance Boost log integration: set Boost::log as imported global property for static builds

--- cmake/LogItBackendBoostlog.cmake
+++ cmake/LogItBackendBoostlog.cmake
@@ -13,4 +13,12 @@ if(NOT TARGET Boost::log)
     message(FATAL_ERROR "Boost CMake config did not provide Boost::log target.")
 endif()
 
+if(NOT LOGIT_BUILD_SHARED_LIB)
+    set_property(TARGET Boost::log PROPERTY IMPORTED_GLOBAL TRUE)
+    get_directory_property(LOGIT_PARENT_DIRECTORY PARENT_DIRECTORY)
+    if(LOGIT_PARENT_DIRECTORY)
+        set_property(DIRECTORY "${LOGIT_PARENT_DIRECTORY}" APPEND PROPERTY LINK_LIBRARIES Boost::log)
+    endif()
+endif()
+
 list(APPEND LOGIT_EXTRA_LINK_LIBS Boost::log)
